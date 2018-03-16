6f93e05d6f8c468700e067e7ae5085f7232b0879
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2018-03-16 19:56:11 -0400

    got the server running

--- server.js
+++ server.js
@@ -2,16 +2,23 @@
 //==========================================
 
 // 3.  express library
-// We are going to use express library to run the server 
+// We are going to use express library to run the server
+var express = require ('express');
 //===================================================================================
 //===================================================================================
 
 // We want the server to run on a port 
 // DEFINE THE PORT
+var PORT = process.env.PORT || 3000;
 //===================================================================================
 // We weant to make an instance of express library
 // INSTANCE FOR EXPRESS
+var app = express();
 //===================================================================================
 //===================================================================================
 
+
 // RUN THE SERVER
+app.listen(PORT, function(){
+    console.log('app listening to PORT:'+ PORT)
+});

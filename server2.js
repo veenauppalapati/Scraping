b77a09b09e4d2e0e70f86d3d97327ce72ead9ed9
veena uppalapati <Veena@veenas-MBP.home>
2018-03-16 17:37:08 -0400

    comments for app to run

---
+++ server2.js
@@ -0,0 +1,22 @@
+// Require all the packages for Web Scraping 
+//===================================================================================
+// 1.  Request package
+// We need request package to make a grab data from certain website requested for
+//===================================================================================
+// 2.  cheerio Package
+// We need cheerio package to manipulate the data we retrieved from a certain website
+//===================================================================================
+
+
+// We want the server to run on a port 
+// Define the port
+//===================================================================================
+// We weant to make an instance of express library
+//===================================================================================
+//===================================================================================
+
+// request for a webpage using the request package
+
+//===================================================================================
+//===================================================================================
+// Run the server

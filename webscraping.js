383f5cb64f90cbca6b572eced28a6dbd41c20948
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2018-03-16 20:16:39 -0400

    webscraping comments

---
+++ webscraping.js
@@ -0,0 +1,15 @@
+// Require all the packages for Web Scraping 
+//===================================================================================
+// 1.  Request package
+// We need request package to makes HTTP request for HTML page
+//===================================================================================
+// 2.  cheerio Package
+// We need cheerio package to parse our HTML and helps us find elements
+//===================================================================================
+
+//Use request and store the elements
+// Load the HTML to cheerio
+// create an empty array to store the scraped results
+// iterate through the results 
+// iterate throught elements that you want
+// push the results to the array
